ac5cca582301714f37806efd5c19e7117e0045c8
Rens Nooren <rens.nooren@philips.com>
2022-08-21 13:28:03 +0200

    Remove link from overview for testing

--- pages/avatars.js
+++ pages/avatars.js
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function Avatars({ avatars }) {
   return (
     <div>
@@ -7,12 +5,9 @@ export default function Avatars({ avatars }) {
       <p>All of the avatars that appeared in the show:</p>
       <ul style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gridGap: '4em' }}>
         {avatars.map(avatar => {
-          const avatarName = avatar.name.toLowerCase().replace(/ /g, '-');
           return (
             <li key={avatar._id}>
-              <Link href={`/characters/${avatarName}`}>
-                <p>{ avatar.name }</p>
-              </Link>
+              <p>{ avatar.name }</p>
               <img src={avatar.photoUrl} width="100%" alt="" />
             </li>
           )
